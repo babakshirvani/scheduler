4ffb0895439985ebc5d6d907f790c6247c4c3bdb
Babak Shirvani <babak@MacBook-Pro.local>
2021-09-30 22:34:31 -0700

    Managing State

--- src/components/Application.js
+++ src/components/Application.js
@@ -71,16 +71,25 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
+  // const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
 
-  const [day, setDay] = useState("Monday");
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
 
   useEffect(() => {
     axios.get("http://localhost:8001/api/days")
-    .then(response => {
-      console.log(response.data)
-      setDays(response.data)
-    })
+      .then(response => {
+        console.log(response.data)
+        setDays(response.data)
+      })
   }, []);
 
   return (
@@ -94,8 +103,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
