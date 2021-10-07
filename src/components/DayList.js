6b7463d8f590f00ef8b5ef093c2702a6a0911fd8
Babak Shirvani <babak@MacBook-Pro.local>
2021-10-06 17:01:15 -0700

    E2E Testing, Add Navigating Days, and Appointment Tests

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,13 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
 
   const day = props.days.map((day) =>
-    <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} />
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
   )
   return (
     <ul>
